1bc1b86a1a7fda1bc2625d6a35cbb44d0bf691e4
MariaConuoha <onuohamaria@gmail.com>
2020-08-27 15:44:03 +0100

    Fixed Transfers completely

--- bankApp.py
+++ bankApp.py
@@ -157,7 +157,16 @@ class Account(User):
         return str(account_num)
 
     ## DEPOSIT
-    def deposit(self, amount, comment = ''):
+    def deposit(self, amount, comment = 'no comment', source = False):
+
+        transaction_label = 'credit'
+
+        if source:
+            transaction_type = 'transfer'
+            source = source.name
+        else:
+            transaction_type = 'deposit'
+            source = self.name
 
         amount = amount.replace(",", "")
 
@@ -167,11 +176,23 @@ class Account(User):
         locale.setlocale(locale.LC_ALL, 'en_US')
         str_balance = locale.format_string("%d", self.balance, grouping=True)
 
+        self.store_history(transaction_type, transaction_label, amount, self.name, comment, source)
+
         print(f'Welldone {self.name} your deposit of ₦{amount} was successful your new balance is ₦{str_balance} ')
 
 
     ##WITHDRAW
-    def withdraw(self, amount, comment = ''):
+    def withdraw(self, amount, comment = 'no comment', collector = False):
+
+        transaction_label = 'debit'
+
+        if collector:
+            transaction_type = 'transfer'
+            collector = collector.name
+        else:
+            transaction_type = 'withdrawal'
+            collector = self.name
+
 
         amount = amount.replace(",", "")
 
@@ -181,36 +202,36 @@ class Account(User):
         locale.setlocale(locale.LC_ALL, 'en_US')
         str_balance = locale.format_string("%d", self.balance, grouping=True)                             ## Adds deposit value to balance
         
-        self.store_history('debit', amount, comment)
+        self.store_history( transaction_type, transaction_label, amount, self.name, comment, collector)
 
-        print(f'{self.name} your deposit of ₦{amount} was successful your new balance is ₦{str_balance} ')
+        print(f'{self.name} your withdrawal of ₦{amount} was successful your new balance is ₦{str_balance} ')
 
 
     ## TRANSFER
-    def transfer(self, recipient, amount, comment = ''):
-        
+    def transfer(self, amount, recipient, comment = ''):
+
+        self.withdraw(amount, comment, recipient)
+        recipient.deposit(amount, comment, self)
+
         amount = amount.replace(",", "")
 
-        self.balance -= int(amount)                          ## Remove transfer amount from sender's balance
+        self.balance -= int(amount)                               ## Adds deposit value to balance
 
-        recipient.balance += int(amount)                          ## Add transfer amount from recipient's balance
         
         locale.setlocale(locale.LC_ALL, 'en_US')
-        str_balance = locale.format_string("%d", self.balance, grouping=True)                             ## Adds deposit value to balance
-        
+        str_balance = locale.format_string("%d", self.balance, grouping=True)  
 
-        self.store_history('debit', amount, comment, recipient.name)
 
         print(f'Congrats {self.name} your transfer of ₦{amount} to {recipient.name} was successful. Your new balance is ₦{str_balance}. ')
 
 
     ## HISTORY
-    def store_history(self, type, amount, comment, receiver = 'same as sender'):
+    def store_history(self, transaction_type, transaction_label, amount, source, comment, receiver = 'same as sender'):
         
         file = open('Financial_statement.csv', 'a')
-        file.write(f'{type}, {self.name}, {amount}, {comment}, {receiver}\n')
+        file.write(f'{transaction_type}, {transaction_label}, {amount}, {source}, {receiver}, {comment}\n')
 
-        print(type, amount, comment, receiver)
+        print(transaction_type, amount, comment, receiver)
 
 
 atha = Account('Atha', 23, 'atha_mg@yahoo.com', '08032134387')
@@ -219,6 +240,6 @@ atha.deposit("100,000,000")
 atha.withdraw('50,000')
 
 bolu = Account('Bolu', 23, 'bolu_mg@yahoo.com', '08032134387')
-atha.transfer(bolu, '25,000', 'flexing')
+atha.transfer ('25,000', bolu, 'flexing')
 
 
